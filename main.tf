2fb093aded52b02e95c14ca97401bf1444f9355d
securitymountaineeringteam4 <72583522+securitymountaineeringteam4@users.noreply.github.com>
2020-10-09 11:15:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,14 @@
 provider "aws" {
-
-version = 3.9
-
-profile = "default"
-
-region = "us-east-2"
-
+  version = 3.9
+  profile = "default"
+  region  = "us-east-2"
 }
 
 module "website_s3_bucket" {
-
-source = "./modules/"
-
-bucket_name = "test-bucket-student-12345"
-
-tags = { Terraform = "true"
-
-Environment = "dev"
-}
-
-}
+  source = "./modules/"
+  bucket_name = "test-bucket-student-12345"
+    tags = {
+    Terraform   = "true"
+        Environment = "dev"
+    }
+ }
