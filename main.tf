5e9c302f0337f1fefa7e0a7ea0ec72bc02bf2b7c
securitymountaineeringteam4 <72583522+securitymountaineeringteam4@users.noreply.github.com>
2020-10-09 11:23:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ provider "aws" {
   version = 3.9
   profile = "default"
   region  = "us-east-2"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
 
 variable "aws_access_key" {}
