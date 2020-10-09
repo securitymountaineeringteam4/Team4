b58095922d5ddbc304ed62431d5137789b5f3a88
securitymountaineeringteam4 <72583522+securitymountaineeringteam4@users.noreply.github.com>
2020-10-09 11:10:12 -0400

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,9 +1,29 @@
-variable "bucket_name" {
-
-description = "Name of the s3 bucket. Must be unique."
-
-type = string
-
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.bucket_name}/*"
+            ]
+        }
+    ]
 }
+EOF
 
-variable "tags" { description = "Tags to set on the bucket." type = map(string) default = {} }
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  tags = var.tags
+}
