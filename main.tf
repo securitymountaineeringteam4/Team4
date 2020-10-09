18319a9ab1dace697849e6f75e2d4ffc97bea352
securitymountaineeringteam4 <72583522+securitymountaineeringteam4@users.noreply.github.com>
2020-10-09 11:00:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,22 @@
 provider "aws" {
-  version = 3.9
-  profile = "default"
-  region  = "us-east-2"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+
+version = 3.9
+
+profile = "default"
+
+region = "us-east-2"
+
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+module "website_s3_bucket" {
 
-resource "aws_s3_bucket" "testing" {
-  bucket = "security-mountaineering-lab-bucket-team4"
+source = "./modules/"
+
+bucket_name = "test-bucket-student-12345"
+
+tags = { Terraform = "true"
+
+Environment = "dev"
+}
+
 }
